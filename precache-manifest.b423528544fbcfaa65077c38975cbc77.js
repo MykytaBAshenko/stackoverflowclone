5f9b0516b4ab221ea0e4bf6a16374690d2a30267
MykytaBAshenko <nikita.bashenko2001@gmail.com>
2020-11-06 17:17:41 +0200

    Updates

---
+++ precache-manifest.b423528544fbcfaa65077c38975cbc77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cd1795e044ae6a18ed44ed69efe3346",
+    "url": "/stackoverflowclone/index.html"
+  },
+  {
+    "revision": "9b8f96a775469935b235",
+    "url": "/stackoverflowclone/static/css/main.73ab90e1.chunk.css"
+  },
+  {
+    "revision": "184885c7de7095ef5b37",
+    "url": "/stackoverflowclone/static/js/2.f40bae45.chunk.js"
+  },
+  {
+    "revision": "00709531c2db38d4440f36ba90fbb900",
+    "url": "/stackoverflowclone/static/js/2.f40bae45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b8f96a775469935b235",
+    "url": "/stackoverflowclone/static/js/main.39443d2c.chunk.js"
+  },
+  {
+    "revision": "abb7e17c6be449a3e1e6",
+    "url": "/stackoverflowclone/static/js/runtime-main.c944343a.js"
+  }
+]);
